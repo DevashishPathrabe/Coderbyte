20240d1b20b31468c157296d40e6d6ee45bec957
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-26 20:41:49 +0530

    Codeland Username Validation
    Here is my Codeland Username Validation Solution...

---
+++ Coderbyte Solutions/Algorithms/Codeland Username Validation.py
@@ -0,0 +1,17 @@
+import re
+def CodelandUsernameValidation(str):
+
+  # code goes here
+  if (not str[0].isalpha()) or (len(str)>25) or (len(str) < 4) or (str[-1] == "_"):
+    return "false"
+  
+  for i in range(len(str)-2):
+      if str[i+1].isalpha() or str[i+1].isdigit() or str[i+1] == "_":
+        pass
+      else:
+        return "false"
+  
+  return "true"
+
+# keep this function call here 
+print(CodelandUsernameValidation(input()))
