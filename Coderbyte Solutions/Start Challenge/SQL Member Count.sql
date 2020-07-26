2c4c9539ea6e9123c4e02061c288cef030b54770
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-26 20:41:20 +0530

    Delete SQL Member Count.sql

--- Coderbyte Solutions/Start Challenge/SQL Member Count.sql
+++
@@ -1,3 +0,0 @@
-/* write your SQL query below */
-
-SELECT ReportsTo, COUNT(ReportsTo) 'Members', ROUND(AVG(Age)) 'Average Age' FROM maintable_G0KI3 WHERE ReportsTo IS NOT NULL GROUP BY ReportsTo ORDER BY ReportsTo;
